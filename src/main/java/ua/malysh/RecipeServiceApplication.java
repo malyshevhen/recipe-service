634937a1e2b2b8f0026167f3561e825c0f86fe2f
Evhen <malysh.evgeniy@gmail.com>
2023-05-25 18:02:16 +0300

    first commit

---
+++ src/main/java/ua/malysh/RecipeServiceApplication.java
@@ -0,0 +1,13 @@
+package ua.malysh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/malysh/RecipeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.malysh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
