8d42e2b019601fcdd56a7fec6f580c0591a9814a
Evhen <malysh.evgeniy@gmail.com>
2023-05-27 11:24:06 +0300

    fix test with datasource configs

--- src/test/java/ua/malysh/RecipeServiceApplicationTests.java
+++ src/test/java/ua/malysh/RecipeServiceApplicationTests.java
@@ -1,9 +1,13 @@
 package ua.malysh;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, properties = "spring.datasource.url=jdbc:tc:postgresql:15:///test_db")
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class RecipeServiceApplicationTests {
 
     @Test
