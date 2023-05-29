b13bbaccec88dd91932a60d06b90aed6d890418a
Evhen <malysh.evgeniy@gmail.com>
2023-05-29 05:29:44 +0300

    add constructor to Ingredient

--- src/main/java/ua/malysh/domain/Ingredient.java
+++ src/main/java/ua/malysh/domain/Ingredient.java
@@ -2,10 +2,14 @@ package ua.malysh.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Ingredient {
 
     @Column(name = "product_id")
