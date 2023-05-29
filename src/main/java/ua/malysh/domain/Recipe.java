1f150c67d8ab96c1586e4def123a3270bd82c397
Evhen <malysh.evgeniy@gmail.com>
2023-05-29 05:30:14 +0300

    add method addIngredients

--- src/main/java/ua/malysh/domain/Recipe.java
+++ src/main/java/ua/malysh/domain/Recipe.java
@@ -1,11 +1,5 @@
 package ua.malysh.domain;
 
-import jakarta.persistence.*;
-import lombok.*;
-import org.hibernate.Hibernate;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 import java.time.LocalDateTime;
@@ -13,13 +7,32 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
-@Table(name = "recipes",
-        indexes = {
-                @Index(name = "idx_recipes_recipe_id", columnList = "recipe_id")},
-        uniqueConstraints = {
-                @UniqueConstraint(name = "uc_recipes_recipe_name", columnNames = {"recipe_name"})})
-@Getter @Setter
+@Table(name = "recipes", indexes = {
+        @Index(name = "idx_recipes_recipe_id", columnList = "recipe_id") }, uniqueConstraints = {
+                @UniqueConstraint(name = "uc_recipes_recipe_name", columnNames = { "recipe_name" }) })
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Recipe {
@@ -27,11 +40,11 @@ public class Recipe {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     @Column(name = "recipe_id",
-            nullable = false)
+                    nullable = false)
     private Long id;
 
     @Column(name = "recipe_name",
-            nullable = false)
+                    nullable = false)
     private String name;
 
     @ElementCollection
@@ -54,6 +67,10 @@ public class Recipe {
     @Column(name = "modified_at")
     @UpdateTimestamp
     private LocalDateTime modifiedAt;
+
+    public void addIngredients(Set<Ingredient> ingredients) {
+        this.ingredients.addAll(ingredients);
+    }
 
     public void addIngredient(Ingredient ingredient) {
         this.ingredients.add(ingredient);
